241db484d34a5670c2308d3215b6f2a0253782fe
sulaimanbaig <engsulaimanbaig@gmailcom>
2020-06-18 07:19:50 +0500

    Customer and Driver tables created
    Customer and Driver tables created

---
+++ models/driver.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define("driver", {
+      id: {
+        type: type.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        required: true
+      },
+      firstName: type.STRING,
+      lastName: type.STRING,
+      address: type.STRING,
+      lattitude: type.STRING,
+      longitude: type.STRING,
+      city: type.STRING,
+      country: type.STRING,
+      phoneNo: type.STRING,
+      imageURl: type.STRING,
+ 
+     
+    });
+  };
+  
