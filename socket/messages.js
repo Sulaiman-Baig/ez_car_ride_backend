1f71b231587c38947b339f1e58b984399ee026e2
sulaimanbaig <engsulaimanbaig@gmailcom>
2020-07-07 14:23:20 +0500

    send to all available drivers

--- socket/messages.js
+++ socket/messages.js
@@ -1,10 +1,18 @@
-module.exports = function(io) {
+module.exports = function (io) {
     io.on('connection', (socket) => {
         console.log('socket is running');
         socket.on('send-data-to-drivers', data => {
+
+            data.forEach(driver => {
+                io.emit(driver.id, driver);
+            });
             // io.emit('refreshPage', {});
             console.log('socket is listening to send-data-to-drivers');
             console.log(data);
+
+
         });
+
+      
     })
 } 
